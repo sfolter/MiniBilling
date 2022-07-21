7f128828e6a51c38f7be1bd2e190664c9307394f
Miroslav Kovachev <miroslav.kovachev@gmail.com>
2022-07-21 15:16:02 +0300

    - updates project definition with more details, setups a gradle project

---
+++ src/main/java/com/github/methodia/minibilling/Main.java
@@ -0,0 +1,13 @@
+package com.github.methodia.minibilling;
+
+/**
+ * @author Miroslav Kovachev
+ * 21.07.2022
+ * Methodia Inc.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //WELCOME TO MINI BILLING!
+    }
+}
