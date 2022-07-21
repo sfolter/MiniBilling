e7339d81d71cbdd705086ef3c97b3d006b8f86d7
Petar <petarhristov235@abv.bg>
2022-07-21 21:17:06 +0300

    Test Commit

--- src/main/java/com/github/methodia/minibilling/PricesFileReader.java
+++ src/main/java/com/github/methodia/minibilling/PricesFileReader.java
@@ -10,7 +10,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class PricesFileReader {
+public class PricesFileReader  implements FileReading{
     String path;
 
     public PricesFileReader(String path) {
