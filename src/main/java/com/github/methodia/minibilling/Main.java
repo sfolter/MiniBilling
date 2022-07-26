809ea7265619e6ed99ec875b63dcaa553a03bcf1
Russev01 <derussev@gmail.com>
2022-07-26 13:23:27 +0300

    documentDate and documentNumber upgrade

--- src/main/java/com/github/methodia/minibilling/Main.java
+++ src/main/java/com/github/methodia/minibilling/Main.java
@@ -9,7 +9,10 @@ import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.DateFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.LinkedHashMap;
 
 public class Main {
@@ -28,32 +31,39 @@ public class Main {
 
 
         JSONObject jsonObject = new JSONObject();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        Calendar cal = Calendar.getInstance();
+
 
 
         for (int i = 0; i < user.getCount(); i++) {
             int diff = user.getReference(i);
+            int min = 10000;
+            int max = 99999;
+
+            int random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
 
             Field changeMap = jsonObject.getClass().getDeclaredField("map");
             changeMap.setAccessible(true);
             changeMap.set(jsonObject, new LinkedHashMap<>());
             changeMap.setAccessible(false);
 
-            jsonObject.put("documentDate", "");
-            jsonObject.put("documentNumber", 10000);
+            jsonObject.put("documentDate", dateFormat.format(cal.getTime()));
+            jsonObject.put("documentNumber", random_int);
             jsonObject.put("consumer", user.getConsumer(i));
             jsonObject.put("reference", user.getPriceList(i));
             jsonObject.put("totalAmount", readings.getAmount(diff - 1));
 
 
             JSONArray courses = new JSONArray(
-                    new String[]{"index: " + user.getReference(i), "quantity: " + readings.getQuantity(i),
+                    new String[]{"index: " + user.getReference(i), "quantity: " + readings.getQuantity(diff-1),
                             "lineStart: " + readings.getStartDate(diff - 1), "lineEnd: " + readings.getEndDate(diff - 1),
                             "product: " + readings.getProduct(i), "price: " + prices.getPrice(0),
                             "priceList: " + user.getPriceList(i), "amount: " + readings.getAmount(diff - 1)});
 
             jsonObject.put("lines", courses);
 
-            //System.out.println(jsonObject.toString(4));
+
 
 
             String path = "C:\\Users\\user\\Desktop\\tasks\\MiniBillingNew\\src\\test\\";
@@ -65,7 +75,7 @@ public class Main {
 
             boolean bool = file.mkdir();
 
-            String txtName = "1000-" + readings.monthBG(readings.getEndDate(i)) + "-" + readings.getYear(readings.getEndDate(i));
+            String txtName = random_int + "-" + readings.monthBG(readings.getEndDate(i)) + "-" + readings.getYear(readings.getEndDate(i));
 
 
             File Dirfile = new File("C:\\Users\\user\\Desktop\\tasks\\MiniBillingNew\\src\\test\\" + folderName + "\\" + txtName + ".txt");
@@ -76,5 +86,7 @@ public class Main {
 
 
         }
+
     }
+
 }
--- src/main/java/com/github/methodia/minibilling/Readings.java
+++ src/main/java/com/github/methodia/minibilling/Readings.java
@@ -5,7 +5,9 @@ import java.io.FileNotFoundException;
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Scanner;
 
 
@@ -19,19 +21,22 @@ public class Readings {
     private ArrayList<Integer> number = new ArrayList<>();
     private ArrayList<String> product = new ArrayList<>();
     private ArrayList<ZonedDateTime> data = new ArrayList<>();
-    private ArrayList<Integer> price = new ArrayList<>();
+    private ArrayList<Integer> quantityFullValue = new ArrayList<>();
     private ArrayList<BigDecimal> quantity = new ArrayList<>();
     private ArrayList<ZonedDateTime> dateStart = new ArrayList<>();
     private ArrayList<ZonedDateTime> dateEnd = new ArrayList<>();
     int counterReadings = 0;
 
     public Readings(ArrayList<Integer> number, ArrayList<String> product,
-                    ArrayList<ZonedDateTime> data, ArrayList<Integer> price, ArrayList<BigDecimal> dateDiff) {
+                    ArrayList<ZonedDateTime> data, ArrayList<Integer> price, ArrayList<BigDecimal> dateDiff,
+                    ArrayList<ZonedDateTime> dateStart, ArrayList<ZonedDateTime> dateEnd) {
         this.number = number;
         this.product = product;
         this.data = data;
-        this.price = price;
+        this.quantityFullValue = price;
         this.quantity = dateDiff;
+        this.dateStart = dateStart;
+        this.dateEnd = dateEnd;
     }
 
 
@@ -60,7 +65,7 @@ public class Readings {
 
             }
             if (counter == 4) {
-                price.add(Integer.parseInt(i));
+                quantityFullValue.add(Integer.parseInt(i));
                 counter = 0;
             }
             counterReadings++;
@@ -71,21 +76,11 @@ public class Readings {
     public Integer getNumber(int i) {
         return this.number.get(i);
     }
-
     public String getProduct(int i) {
         return this.product.get(i);
     }
-
     public ZonedDateTime getData(int i) {
         return this.data.get(i);
-    }
-
-    public Integer getPrice(int i) {
-        return this.price.get(i);
-    }
-
-    public Integer getCount() {
-        return this.counterReadings / 4;
     }
 
     public void Quantity() {
@@ -94,7 +89,7 @@ public class Readings {
             for (int j = 10; j < 20; j++) {
                 if (number.get(i) == counter && number.get(i) == number.get(j)) {
                     counter++;
-                    BigDecimal haveToPay = BigDecimal.valueOf(Math.max(price.get(i), price.get(j)) - Math.min(price.get(i), price.get(j)));
+                    BigDecimal haveToPay = BigDecimal.valueOf(Math.max(quantityFullValue.get(i), quantityFullValue.get(j)) - Math.min(quantityFullValue.get(i), quantityFullValue.get(j)));
                     quantity.add(haveToPay);
                     dateStart.add(data.get(i));
                     dateEnd.add(data.get(j));
@@ -123,54 +118,55 @@ public class Readings {
 
     public String monthBG(ZonedDateTime date) {
 
-        String month = date.getMonth().toString();
-//        DateTimeFormatter bulgarianMonthFormetter =
-//                DateTimeFormatter.ofPattern("MMMM", new Locale("bg"));
-//        String formattedMonth = date.format(bulgarianMonthFormetter);
-//        return formattedMonth;
-        switch (month) {
-            case "JANUARY":
-                month = "Януари";
-                break;
-            case "FEBRUARY":
-                month = "Февруари";
-                break;
-            case "MARCH":
-                month = "Март";
-                break;
-            case "APRIL":
-                month = "Април";
-                break;
-            case "MAY":
-                month = "Май";
-                break;
-            case "JUNE":
-                month = "Юни";
-                break;
-            case "JULY":
-                month = "Юли";
-                break;
-            case "AUGUST":
-                month = "Август";
-                break;
-            case "SEPTEMBER":
-                month = "Септември";
-                break;
-            case "OCTOBER":
-                month = "Октомври";
-                break;
-            case "NOVEMBER":
-                month = "Ноември";
-                break;
-            case "DECEMBER":
-                month = "Декември";
-                break;
-            default:
-                break;
-
-        }
-
-        return month;
+       // String month = date.getMonth().toString();
+        DateTimeFormatter bulgarianMonthFormetter =
+                DateTimeFormatter.ofPattern("MMMM", new Locale("bg"));
+        String formattedMonth = date.format(bulgarianMonthFormetter).substring(0, 1).toUpperCase()
+                + date.format(bulgarianMonthFormetter).substring(1);
+        return formattedMonth;
+//        switch (month) {
+//            case "JANUARY":
+//                month = "Януари";
+//                break;
+//            case "FEBRUARY":
+//                month = "Февруари";
+//                break;
+//            case "MARCH":
+//                month = "Март";
+//                break;
+//            case "APRIL":
+//                month = "Април";
+//                break;
+//            case "MAY":
+//                month = "Май";
+//                break;
+//            case "JUNE":
+//                month = "Юни";
+//                break;
+//            case "JULY":
+//                month = "Юли";
+//                break;
+//            case "AUGUST":
+//                month = "Август";
+//                break;
+//            case "SEPTEMBER":
+//                month = "Септември";
+//                break;
+//            case "OCTOBER":
+//                month = "Октомври";
+//                break;
+//            case "NOVEMBER":
+//                month = "Ноември";
+//                break;
+//            case "DECEMBER":
+//                month = "Декември";
+//                break;
+//            default:
+//                break;
+//
+//        }
+//
+//        return month;
     }
 
     public Integer getYear(ZonedDateTime date) {
