6b231e59860905caefd1261987a856fd45a34d23
Petar Hristov <petarhristov235@abv.bg>
2022-07-26 13:48:00 +0300

    Task is ready. Have to do the Unit Test and refactor the code.

--- src/main/java/com/github/methodia/minibilling/Main.java
+++ src/main/java/com/github/methodia/minibilling/Main.java
@@ -9,17 +9,28 @@ import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.ZonedDateTime;
+import java.time.chrono.ChronoZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
 import java.util.*;
 import java.lang.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         String outputPath = scanner.nextLine();
-        //    String dateToReporting=scanner.nextLine();
-        //    DateTimeFormatter endDateFormatter=DateTimeFormatter.ofPattern("yy-MM" );
-        //  LocalDate localDate=LocalDate.parse(dateToReporting,endDateFormatter);
+        String dateToReporting=scanner.nextLine();
+
+        DateTimeFormatter formatterBorderTime = new DateTimeFormatterBuilder()
+                .appendPattern("yy-MM")
+                .parseDefaulting(ChronoField.DAY_OF_MONTH, 1)
+                .toFormatter();
+        LocalDate borderTime=LocalDate.parse(dateToReporting,formatterBorderTime);
+        //ZonedDateTime borderTime=ZonedDateTime.parse(dateToReporting,formatterBorderTime);
+
+
         Path resourceDirectory = Paths.get("src", "test", "resources", "sample1", "input");
         final String readingsPath = resourceDirectory + "\\" + "readings.csv";
 
@@ -45,59 +56,62 @@ public class Main {
             Readings firstReadingForUser = readingsForUser.get(0);
             Readings lastReadingForUser = readingsForUser.get(readingsForUser.size() - 1);
             Prices price= prices.get(firstReadingForUser.getProduct());
-            double quantity = lastReadingForUser.getMetrics() - firstReadingForUser.getMetrics();
-            lastReadingForUser.getMetrics();
-            firstReadingForUser.getMetrics();
-            prices.get(readings.get("gas"));
-            double totalAmount=quantity*price.getPrice();
+            int lastReadingForUserDateDay=lastReadingForUser.getDate().getDayOfMonth();
+            Month lastReadingForUserDateMonth=lastReadingForUser.getDate().getMonth();
+            int lastReadingForUserDateYear=lastReadingForUser.getDate().getYear();
+            LocalDate lastReadingForUserInLocalDate=LocalDate.of(lastReadingForUserDateYear,lastReadingForUserDateMonth,lastReadingForUserDateDay);
+            if(lastReadingForUserInLocalDate.isBefore(borderTime)) {
+                double quantity = lastReadingForUser.getMetrics() - firstReadingForUser.getMetrics();
+                lastReadingForUser.getMetrics();
+                firstReadingForUser.getMetrics();
+                prices.get(readings.get("gas"));
+                double totalAmount = quantity * price.getPrice();
 
-                        Lines line = new Lines();
-                        line.index =1 ;
-                        line.quantity = quantity;
-                        line.lineStart = String.valueOf(firstReadingForUser.getDate());
-                        line.lineEnd = String.valueOf(lastReadingForUser.getDate());
-                        line.product = firstReadingForUser.getProduct();
-                        line.price = price.getPrice();
-                        line.priceList = user.getNumberPricingList();
-                        line.amount = quantity * price.getPrice();
-                        double totalAmountCounter = 0;
-                        outputClass.lines.add(line);
-
-
-
-                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
-                        outputClass.documentDate = ZonedDateTime.now().format(formatter);
-                        int documentNumber=i*10000;
-                        outputClass.documentNumber =String.valueOf(documentNumber);
-                        outputClass.consumer = user.getName();
-                        outputClass.reference = user.getReferentNumber();
-                        outputClass.totalAmount = line.amount;
-                        reportTime = String.valueOf(lastReadingForUser.getDate());
+                Lines line = new Lines();
+                line.index = 1;
+                line.quantity = quantity;
+                line.lineStart = String.valueOf(firstReadingForUser.getDate());
+                line.lineEnd = String.valueOf(lastReadingForUser.getDate());
+                line.product = firstReadingForUser.getProduct();
+                line.price = price.getPrice();
+                line.priceList = user.getNumberPricingList();
+                line.amount = quantity * price.getPrice();
+                double totalAmountCounter = 0;
+                outputClass.lines.add(line);
 
 
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
+                outputClass.documentDate = ZonedDateTime.now().format(formatter);
+                int documentNumber = i * 10000;
+                outputClass.documentNumber = String.valueOf(documentNumber);
+                outputClass.consumer = user.getName();
+                outputClass.reference = user.getReferentNumber();
+                outputClass.totalAmount = line.amount;
+                reportTime = String.valueOf(lastReadingForUser.getDate());
 
-            //todo kato e gotovo pravish edin output class (example below), mapvash gotovoto info kum nego
-            //finish with outplustClass.jsonify() or toJson() depending on what library you are using
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            String json = gson.toJson(outputClass);
-            String folderPath = outputPath + "\\" + user.getName() + "-" + user.getReferentNumber();
-            File creatingFolders = new File(folderPath);
-            boolean bool2 = creatingFolders.mkdirs();
 
-            Date jud = new SimpleDateFormat("yy-MM-dd").parse(reportTime);
-            String month = DateFormat.getDateInstance(SimpleDateFormat.LONG, new Locale("bg")).format(jud);
-            String[] splitDate = month.split("\\s+");
-            String monthInCyrilic = splitDate[1];
+                //todo kato e gotovo pravish edin output class (example below), mapvash gotovoto info kum nego
+                //finish with outplustClass.jsonify() or toJson() depending on what library you are using
+                Gson gson = new GsonBuilder().setPrettyPrinting().create();
+                String json = gson.toJson(outputClass);
+                String folderPath = outputPath + "\\" + user.getName() + "-" + user.getReferentNumber();
+                File creatingFolders = new File(folderPath);
+                boolean bool2 = creatingFolders.mkdirs();
 
-            String jsonFilePath = folderPath + "\\" + outputClass.documentNumber + "-" + monthInCyrilic + ".json";
-            File creatingFiles = new File(jsonFilePath);
-            creatingFiles.createNewFile();
-            try (PrintWriter out = new PrintWriter(new FileWriter(jsonFilePath))) {
-                out.write(json.toString());
-            } catch (Exception e) {
-                e.printStackTrace();
+                Date jud = new SimpleDateFormat("yy-MM-dd").parse(reportTime);
+                String month = DateFormat.getDateInstance(SimpleDateFormat.LONG, new Locale("bg")).format(jud);
+                String[] splitDate = month.split("\\s+");
+                String monthInCyrilic = splitDate[1];
+
+                String jsonFilePath = folderPath + "\\" + outputClass.documentNumber + "-" + monthInCyrilic + ".json";
+                File creatingFiles = new File(jsonFilePath);
+                creatingFiles.createNewFile();
+                try (PrintWriter out = new PrintWriter(new FileWriter(jsonFilePath))) {
+                    out.write(json.toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
-
             outputClass = new OutputClass();
             i++;
         }
