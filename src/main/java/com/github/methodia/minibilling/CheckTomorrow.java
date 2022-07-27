e86f501ea6e60359d4a1caec7d3a1016712e4f54
Petar Hristov <petarhristov235@abv.bg>
2022-07-27 17:57:46 +0300

    Try to do changing periods algorithm.

--- src/main/java/com/github/methodia/minibilling/CheckTomorrow.java
+++ src/main/java/com/github/methodia/minibilling/CheckTomorrow.java
@@ -11,11 +11,7 @@ public class CheckTomorrow {
         // resource: https://beginnersbook.com/2017/10/java-8-calculate-days-between-two-dates/
 
         //24-May-2017, change this to your desired Start Date
-            LocalDate dateBefore = LocalDate.of(2017, Month.MAY, 24);
-            //29-July-2017, change this to your desired End Date
-            LocalDate dateAfter = LocalDate.of(2017, Month.JULY, 29);
-            long noOfDaysBetween = ChronoUnit.DAYS.between(dateBefore, dateAfter);
-            System.out.println(noOfDaysBetween);
+
 
 
         /*long noOfDaysBetween = DAYS.between(startDate, endDate);
