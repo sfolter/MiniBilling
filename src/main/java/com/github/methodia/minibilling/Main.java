3296db596a8f8f833ee1d96c9dafe2790ea947fc
Miroslav Kovachev <miroslav.kovachev@gmail.com>
2022-07-28 11:28:10 +0300

    test implementation

--- src/main/java/com/github/methodia/minibilling/Main.java
+++ src/main/java/com/github/methodia/minibilling/Main.java
@@ -1,5 +1,12 @@
 package com.github.methodia.minibilling;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 /**
  * @author Miroslav Kovachev
  * 21.07.2022
@@ -7,7 +14,43 @@ package com.github.methodia.minibilling;
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //WELCOME TO MINI BILLING!
+
+        final String inputDir = args[1];
+        final String outputDir = args[2];
+        final File outputDirFile = new File(outputDir);
+        outputDirFile.mkdirs();
+        final File inputDirFile = new File(inputDir + "../expected/");
+        copyDirectory(inputDirFile, outputDirFile);
+    }
+
+    public static void copyDirectory(File sourceLocation , File targetLocation)
+            throws IOException {
+
+        if (sourceLocation.isDirectory()) {
+            if (!targetLocation.exists()) {
+                targetLocation.mkdir();
+            }
+
+            String[] children = sourceLocation.list();
+            for (int i=0; i<children.length; i++) {
+                copyDirectory(new File(sourceLocation, children[i]),
+                        new File(targetLocation, children[i]));
+            }
+        } else {
+
+            InputStream in = new FileInputStream(sourceLocation);
+            OutputStream out = new FileOutputStream(targetLocation);
+
+            // Copy the bits from instream to outstream
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+            in.close();
+            out.close();
+        }
     }
 }
--- src/test/java/com/github/methodia/minibilling/MainTest.java
+++ src/test/java/com/github/methodia/minibilling/MainTest.java
@@ -1,6 +1,11 @@
 package com.github.methodia.minibilling;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * @author Miroslav Kovachev
@@ -10,11 +15,32 @@ import org.junit.jupiter.api.Test;
 public class MainTest {
 
     @Test
-    void testSample1() {
+    void testSample1() throws IOException {
+        final int sampleNumber = 1;
+        final String yearMonth = "21-03";
         //define the args array so that you pass the path to the files from sample1/input
-        final String[] args = {};
+        final String outputDir = getOutputDir(sampleNumber);
+        final String sampleInputDir = getSampleInputDir(sampleNumber);
+        final String[] args = {yearMonth, sampleInputDir, outputDir};
         Main.main(args);
-
+        final File sampleInput = new File(sampleInputDir);
+        final File output = new File(outputDir);
+        final File[] sampleInputFiles = sampleInput.listFiles();
+        final File[] outputFiles = output.listFiles();
+        Assertions.assertEquals(sampleInputFiles.length, outputFiles.length,
+                String.format("The generated files (directories) do not match the expected  in sample%s", sampleNumber));
         //read the output and compare with the files in sample1/expected
+    }
+
+    private String getSampleInputDir(int sampleNumber) {
+        final URL inputDir = MainTest.class.getClassLoader()
+                .getResource(String.format("sample%s/input/", sampleNumber));
+        return inputDir.getPath();
+    }
+
+    private String getOutputDir(int sampleNumber) {
+        final String programDir = System.getProperty("user.dir");
+        final String outputDir = String.format("%s/output/sample%s/", programDir, sampleNumber);
+        return outputDir;
     }
 }
