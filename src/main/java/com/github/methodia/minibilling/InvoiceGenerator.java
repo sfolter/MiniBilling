b0445537ac222427bbd261c53690cf37d8f11d59
viktoriavelikova <viktoriaa_velikovaa@abv.bg>
2022-08-10 11:36:57 +0300

    Added Vat.

--- src/main/java/com/github/methodia/minibilling/InvoiceGenerator.java
+++ src/main/java/com/github/methodia/minibilling/InvoiceGenerator.java
@@ -1,6 +1,7 @@
 package com.github.methodia.minibilling;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -18,33 +19,63 @@ public class InvoiceGenerator {
         this.prices = prices;
     }
 
-    public Invoice generate(long documentNumber,String borderTime) {
+    public Invoice generate(long documentNumber, String borderTime) {
         ProportionalMeasurementDistributor proportionalMeasurementDistributor = new ProportionalMeasurementDistributor(measurements, prices);
         Collection<QuantityPricePeriod> quantityPricePeriods = proportionalMeasurementDistributor.distribute();
 
         List<InvoiceLine> invoiceLines = new ArrayList<>();
+        List<Vat> vat = new ArrayList<>();
         int counter = 1;
         BigDecimal totalBigDecimal = BigDecimal.ZERO;
+        BigDecimal totalAmountWithVat = BigDecimal.ZERO;
+        LocalDate borderDate = Formatter.parseBorder(borderTime);
+
         for (QuantityPricePeriod qpp : quantityPricePeriods) {
-            int index = counter++;
-            BigDecimal quantity = qpp.getQuantity();
-            LocalDateTime start = qpp.getStart().toLocalDateTime();
-            LocalDateTime end = qpp.getEnd().toLocalDateTime();
-            String product = qpp.getPrice().getProduct();
-            BigDecimal price = qpp.getPrice().getValue();
-            int priceList = user.getPriceListNumber();
-            BigDecimal amount = qpp.getQuantity().multiply(qpp.getPrice().getValue());
-            totalBigDecimal = totalBigDecimal.add(amount);
-            LocalDate borderDate = Formatter.parseBorder(borderTime);
+
             if (qpp.getEnd().toLocalDate().isBefore(borderDate)) {
-                invoiceLines.add(new InvoiceLine(index, quantity, start, end, product, price, priceList, amount));
+
+                int index = counter++;
+                InvoiceLine invoiceLine = createInvoiceLine(index, qpp);
+                totalBigDecimal = totalBigDecimal.add(invoiceLine.getAmount()).stripTrailingZeros();
+                BigDecimal vatAmount = totalBigDecimal.multiply(BigDecimal.valueOf(0.2)).setScale(2, RoundingMode.HALF_UP)
+                        .stripTrailingZeros();
+                totalAmountWithVat = totalBigDecimal.add(vatAmount).stripTrailingZeros();
+                invoiceLines.add(invoiceLine);
+                Vat vat1=createVat(invoiceLine);
+                vat.add(vat1);
             }
         }
         LocalDateTime documentDate = LocalDateTime.now();
-        String docNumber=String.valueOf(documentNumber);
+        String docNumber = String.valueOf(documentNumber);
         String consumer = user.getName();
         String reference = user.getRef();
-        return new Invoice(documentDate, docNumber, consumer, reference, totalBigDecimal, invoiceLines);
+
+
+        return new Invoice(documentDate, docNumber, consumer, reference, totalBigDecimal, totalAmountWithVat, invoiceLines, vat);
     }
 
+    private InvoiceLine createInvoiceLine(int lineIndex, QuantityPricePeriod qpp) {
+
+        BigDecimal quantity = qpp.getQuantity();
+        LocalDateTime start = qpp.getStart().toLocalDateTime();
+        LocalDateTime end = qpp.getEnd().toLocalDateTime();
+        String product = qpp.getPrice().getProduct();
+        BigDecimal price = qpp.getPrice().getValue();
+        int priceList = user.getPriceListNumber();
+        BigDecimal amount = qpp.getQuantity().multiply(qpp.getPrice().getValue()).setScale(2, RoundingMode.HALF_UP).stripTrailingZeros();
+
+        return new InvoiceLine(lineIndex, quantity, start, end,
+                product, price, priceList, amount);
+    }
+
+    private Vat createVat(InvoiceLine invoiceLine) {
+
+        List<Integer> vattedLines = new ArrayList<>();
+        vattedLines.add(invoiceLine.getIndex());
+
+        BigDecimal vatAmount = invoiceLine.getAmount().multiply(BigDecimal.valueOf(0.2)).setScale(2, RoundingMode.HALF_UP)
+                .stripTrailingZeros();
+
+        return new Vat(invoiceLine.getIndex(), vattedLines, vatAmount);
+    }
 }
