53f06f1a9f034afea71e9cf86cf5aab4aaa809f5
viktoriavelikova <viktoriaa_velikovaa@abv.bg>
2022-08-30 16:04:28 +0300

    VatGeneratorTest

---
+++ src/test/java/com/github/methodia/minibilling/VatGeneratorTest.java
@@ -0,0 +1,61 @@
+package com.github.methodia.minibilling;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+
+class VatGeneratorTest {
+    @Test
+    void vatForLine(){
+        final InvoiceLine invoiceLine = getInvoiceLine();
+        final List<InvoiceLine> invoiceLines = new ArrayList<>();
+        invoiceLines.add(invoiceLine);
+        final VatGenerator vatGenerator= new VatGenerator(ExampleInputInformation.vatPercentages());
+        final List<Vat> vats = vatGenerator.generate(invoiceLines, Collections.emptyList());
+        Assertions.assertEquals(new BigDecimal("19.2"),vats.get(0).getAmount(),
+                "Amount of vat is not correct.");
+        Assertions.assertEquals(new BigDecimal("6.4"),vats.get(1).getAmount(),
+                "Amount of vat is not correct.");
+
+    }
+    InvoiceLine getInvoiceLine(){
+        return new InvoiceLine(1, new BigDecimal("100"),
+                LocalDateTime.of(2021, Month.APRIL, 3, 20, 20, 20),
+                LocalDateTime.of(2021, Month.MAY, 3, 20, 20, 20),
+                "gas", new BigDecimal("1.6"), 1, new BigDecimal("160"));
+    }
+    @Test
+    void vatForTaxStanding(){
+        final List<InvoiceLine>invoiceLines= new ArrayList<>();
+        invoiceLines.add(getInvoiceLine());
+       final TaxStandingGenerator taxStandingGenerator= new TaxStandingGenerator(invoiceLines);
+        final List<Tax> taxes = taxStandingGenerator.generate();
+        final VatGenerator vatGenerator= new VatGenerator(ExampleInputInformation.vatPercentages());
+        final List<Vat> vats = vatGenerator.generate(Collections.emptyList(),taxes);
+        Assertions.assertEquals(new BigDecimal("9.92"),vats.get(0).getAmount(),
+                "Amount of vat is not correct.");
+    }
+    @Test
+    void vatForLineAndTax(){
+        final List<InvoiceLine>invoiceLines= new ArrayList<>();
+        invoiceLines.add(getInvoiceLine());
+        final TaxStandingGenerator taxStandingGenerator= new TaxStandingGenerator(invoiceLines);
+        final List<Tax> taxes = taxStandingGenerator.generate();
+        final VatGenerator vatGenerator= new VatGenerator(ExampleInputInformation.vatPercentages());
+        final List<Vat> vats = vatGenerator.generate(invoiceLines,taxes);
+        Assertions.assertEquals(new BigDecimal("19.2"),vats.get(0).getAmount(),
+                "Amount of vat is not correct.");
+        Assertions.assertEquals(new BigDecimal("6.4"),vats.get(1).getAmount(),
+                "Amount of vat is not correct.");
+        Assertions.assertEquals(new BigDecimal("9.92"),vats.get(2).getAmount(),
+                "Amount of vat is not correct.");
+    }
+}
